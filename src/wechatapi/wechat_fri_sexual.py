0d9e1092b6faa4b4900161a0eea2fdd696f651d4
twb0223 <twb0223@gmail.com>
2017-08-04 19:37:28 +0800

    add qrcode

--- src/wechatapi/wechat_fri_sexual.py
+++ src/wechatapi/wechat_fri_sexual.py
@@ -1,7 +1,7 @@
 # coding:utf-8
 import itchat
 # 使用echarts，加上这段
-from echarts import Echart, Legend, Pie
+# from echarts import Echart, Legend, Pie
 
 
 '''
@@ -31,18 +31,18 @@ for i in friends[1:]:
 # 总数算上，好计算比例啊～
 total = len(friends[1:])
 
-# 好了，打印结果
+# # 好了，打印结果
 print(u"男性好友：%.2f%%" % (float(male) / total * 100))
 print(u"女性好友：%.2f%%" % (float(female) / total * 100))
 print(u"未填性别：%.2f%%" % (float(other) / total * 100))
-
-chart = Echart(u'%s的微信好友性别比例' % (friends[0]['NickName']), 'from WeChat')
-chart.use(Pie('WeChat',
-              [{'value': male, 'name': u'男性 %.2f%%' % (float(male) / total * 100)},
-               {'value': female, 'name': u'女性 %.2f%%' % (float(female) / total * 100)},
-               {'value': other, 'name': u'其他 %.2f%%' % (float(other) / total * 100)}],
-              radius=["50%", "70%"]))
-chart.use(Legend(["male", "female", "other"]))
-del chart.json["xAxis"]
-del chart.json["yAxis"]
-chart.plot()
+#
+# chart = Echart(u'%s的微信好友性别比例' % (friends[0]['NickName']), 'from WeChat')
+# chart.use(Pie('WeChat',
+#               [{'value': male, 'name': u'男性 %.2f%%' % (float(male) / total * 100)},
+#                {'value': female, 'name': u'女性 %.2f%%' % (float(female) / total * 100)},
+#                {'value': other, 'name': u'其他 %.2f%%' % (float(other) / total * 100)}],
+#               radius=["50%", "70%"]))
+# chart.use(Legend(["male", "female", "other"]))
+# del chart.json["xAxis"]
+# del chart.json["yAxis"]
+# chart.plot()

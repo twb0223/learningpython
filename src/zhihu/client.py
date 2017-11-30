12bbce7c45398b200b907ee8a49dc69057ead517
twb0223 <twb0223@gmail.com>
2017-11-30 17:15:48 +0800

    a

---
+++ src/zhihu/client.py
@@ -0,0 +1,26 @@
+# 导入 socket、sys 模块
+import socket
+import sys
+
+# 创建 socket 对象
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM,) 
+
+# 获取本地主机名
+host = socket.gethostname() 
+
+# 设置端口好
+port = 9999
+
+# 连接服务，指定主机和端口
+s.connect((host, port))
+
+# 接收小于 1024 字节的数据
+msg = s.recv(1024)
+
+msg2='hi'+ "\r\n"
+
+s.send(msg2.encode('utf-8'))
+
+s.close()
+
+print (msg.decode('utf-8'))
